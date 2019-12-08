811f43e8465b2ce49e07b870abf27d46c4ca8a05
Betta-Lyon-Delsordo <lyondelsordo.betta@gmail.com>
2019-12-08 15:50:44 +0100

    Add get_integer()

--- inout.h
+++ inout.h
@@ -5,4 +5,5 @@ void flat(char param, int line_size);
 void headline (char *msg, char param, int line_size);
 int confirm();
 void get_string(char *str, int max, char *msg);
+int get_integer(int max, char *msg);
 #endif
